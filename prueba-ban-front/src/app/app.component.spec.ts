c3dcbc2a61b72ab2ed1efe5c62b870abf8ce392d
Hernan Inga <hernan.inga@iess.gob.ec>
2025-05-06 14:57:39 -0500

    mensajes de campos obligatorios validados enel front

--- prueba-ban-front/src/app/app.component.spec.ts
+++ prueba-ban-front/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'protecto-base'`, () => {
+  it(`should have as title 'prueba-banco'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('protecto-base');
+    expect(app.title).toEqual('prueba-banco');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('protecto-base app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('prueba-banco app is running!');
   });
 });
